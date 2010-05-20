1a058a11ca5fc6bbad2ac4cbf582839f7d1f8fd3
Alexander Dorofeev <aka.spin@gmail.com>
2010-05-20 06:36:41 +0600

    Add test for cache

---
+++ test/test-cache.js
@@ -0,0 +1,29 @@
+var sys = require("sys");
+var assert = require("assert");
+var cache = require("../lib/cache");
+
+var renders = 0;
+var gets = 0;
+
+for (var i = 0; i < 4; i++) {
+	var key = i.toString();
+	for (var j = 0; j < 1000; j++) {
+		cache(key, 
+				function(value) {
+					if (value == key) {
+						gets++;
+					}
+				}, 
+				function(callback) {
+					setTimeout(function(){
+						renders++;
+						callback(key);
+					}, 200);
+				});
+	}
+}
+
+process.addListener("exit", function () {
+	assert.equal(renders, 4);
+	assert.equal(gets, 4000);
+});
